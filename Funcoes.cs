22330beeca059150b4f82fffb933810fd1ed349e
Kaue Ramos <ramoskauegriss@gmail.com>
2023-04-05 17:31:51 -0300

    TrianguloPOO

---
+++ Funcoes.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrianguloPOO.ConsoleApp
+{
+    internal class Funcoes
+    {
+        public void validacaoDados(int Esq, int Dir, int Inf, bool valido)
+        {
+            int Conta1, Conta2, Conta3;
+
+            Conta1 = Esq + Dir;
+            Conta2 = Inf + Dir;
+            Conta3 = Esq + Inf;
+
+            if (Conta1 <= Inf || Conta2 <= Esq || Conta3 <= Dir)
+            {
+                Console.WriteLine();
+
+                Console.WriteLine("Valores Inválidos: É necessário que a medida de " +
+                    "qualquer um dos lados seja menor que a soma dos outros dois lados");
+
+                Console.Write("Pressione qualquer botão...");
+                Console.ReadKey();
+
+                Console.Clear();
+
+                valido = false;
+            }
+            else
+            {
+                valido = true;
+            }
+
+            return;
+        }
+
+        public void verificacaoDados(int Esq, int Dir, int Inf)
+        {
+            if (Dir == Esq && Dir != Inf)
+            {
+                Console.WriteLine("Este é um triângulo isósceles");
+            }
+            else if (Esq == Inf && Dir == Inf && Esq == Dir)
+            {
+                Console.WriteLine("Este é um triângulo equilátero");
+            }
+            else if (Dir != Esq && Dir != Inf && Esq != Inf)
+            {
+                Console.WriteLine("Este é um triângulo escaleno");
+            }
+            else
+            {
+                Console.WriteLine("Nada encontrando");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Drawing;
+
+namespace TrianguloPOO.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcoes F = new Funcoes();
+            int Esq, Dir, Inf;
+            int Conta1, Conta2, Conta3;
+            bool valido = false;
+
+            do
+            {
+                try
+                {
+                    Console.Write("Valor da face da direita: ");
+                    Dir = Convert.ToInt32(Console.ReadLine());
+
+                    Console.Write("Valor da face da esquerda: ");
+                    Esq = Convert.ToInt32(Console.ReadLine());
+
+                    Console.Write("Valor da face inferior: ");
+                    Inf = Convert.ToInt32(Console.ReadLine());
+
+                    F.validacaoDados(Dir, Esq, Inf, valido);
+
+                    if (valido == true)
+                    {
+                        F.verificacaoDados(Dir, Esq, Inf);
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.Write("Preencha os campos com valores válidos...");
+                    Console.ReadKey();
+
+                    Console.Clear();
+                }
+
+            } while (true);
+        }     
+    }
+}
